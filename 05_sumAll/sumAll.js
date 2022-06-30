967ed2ee457d41141f290c39741645573f2c171a
ada_ip <ada.iglesias.pasamontes@gmail.com>
2022-06-30 20:08:01 +0200

    Solve exercises 1-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+	// Invalid arguments checking
+	if (typeof num1 !== "number" || typeof num2 !== "number") {
+		return "ERROR";
+	}
+	if (num1 < 0 || num2 < 0) {
+		return "ERROR";
+	}
 
+	// Variables for storing the min number of the two, the maximum number and the sum aff all the numbers between both numbers
+	let sum = 0;
+	let min = 0;
+	let max = 0;
+	if (num1 < num2) {
+		min = num1;
+		max = num2;
+	} else {
+		min = num2;
+		max = num1;
+	}
+
+	for (let i = min; i <= max; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (degrees) {
+	let celsius = ((degrees - 32) * 5) / 9;
+	return Number(celsius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function (degrees) {
+	let fahrenheit = (degrees * 9) / 5 + 32;
+	return Number(fahrenheit.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+	ftoc,
+	ctof,
 };
